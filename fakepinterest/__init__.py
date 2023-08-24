d55ba66e7e30b93c668c5331759d10d7db2bd643
Luis Gomes <luis.gomes_@outlook.com>
2023-08-23 23:10:53 -0300

    criação do banco de dados e formularios

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -1,5 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///comunidade.db"
+app.config['SECRET_KEY'] = 'abbbb114a4d2644a86af9f5f56ae49d1'
+
+database = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'homepage'
+
 
 from fakepinterest import routes
--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,12 +1,25 @@
 from flask import render_template, url_for
 from fakepinterest import app
+from flask_login import login_required
+from fakepinterest.forms import FormCriarConta, FormLogin
 
 
-@app.route("/")
+
+@app.route("/", methods=['GET', 'POST'])
 def homepage():
-    return render_template("homepage.html")
+    formlogin = FormLogin()
+    return render_template("homepage.html", form=formlogin)
 
 
+
+@app.route("/criarconta", methods=['GET', 'POST'])
+def criarconta():
+    formcriarconta = FormCriarConta()
+    return  render_template("criarconta.html", form=formcriarconta)
+
+
+
+@login_required
 @app.route("/perfil/<usuario>")
 def perfil(usuario):
     return render_template("perfil.html", usuario=usuario)
